8063b341117207dd7bb9d865d4ca0ec5dfa5711a
Claire Bohlen <clairebohlen@gmail.com>
2021-01-16 18:53:57 -0700

    adding JS Search Bar

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 const panels = document.querySelectorAll('.panel');
+const icon = document.querySelector('.icon');
+const input = document.querySelector('.input');
+const search = document.querySelector('.search')
+const btn = document.querySelector('.btn')
 
+//Event Listeners
 panels.forEach((panel)=>{
     panel.addEventListener('click', ()=>{
         removeActiveClasses();
         panel.classList.add('active');
     });
 });
+
+btn.addEventListener('click', () => {
+    search.classList.toggle('active')
+    input.focus()
+})
 
 function removeActiveClasses(){
     panels.forEach(panel => {
