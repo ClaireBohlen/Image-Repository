77f517cc593eaf0901c432c8052829eef975ece6
Claire Bohlen <clairebohlen@gmail.com>
2021-01-17 17:50:25 -0700

    fixing JS DOM els

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-const panels = document.querySelectorAll('.panel');
 const icon = document.querySelector('.icon');
 const input = document.querySelector('.input');
 const search = document.querySelector('.search')
 const btn = document.querySelector('.btn')
 const gallery = document.querySelector('.gallery');
+const panels = document.querySelectorAll('.panel');
 
 //API AUTH
 const auth = "563492ad6f91700001000001dad42820d13a4564af0a88ab82c69e51";
@@ -26,25 +26,38 @@ async function fetchApi(url) {
     return data;
   }
 
+
+  //Generating DOM with API Data
   function generatePictures(data) {
     data.photos.forEach((photo) => {
       const galleryImg = document.createElement("div");
-      galleryImg.classList.add("panel");
+      galleryImg.classList.add("panel1");
       galleryImg.innerHTML = `
       <h3>${photo.photographer}</h3>
       <a href=${photo.src.original}>Download</a>
-     
-      <img src=${photo.src.large}></img>
-      
-      `;
-      gallery.appendChild(galleryImg);
+     <img class="panel" src=${photo.src.large}></img>`;
+     gallery.appendChild(galleryImg);
+
+
+     //Click Event 
+      const panels = document.querySelectorAll('.panel');
+      panels.forEach((panel)=>{
+        panel.addEventListener('click', ()=>{
+            console.log('HI')
+            removeActiveClasses();
+            panel.classList.add('active');
+        });
+        function removeActiveClasses(){
+            panels.forEach(panel => {
+                panel.classList.remove('active');
+            })
+        }
     });
-  }
-//   <div class="gallery-info">
-//       <p>${photo.photographer}</p>
-//       <a href=${photo.src.original}>Download</a>
-//       </div>
-//       <img src=${photo.src.large}></img>
+   });
+  };
+
+  
+
 
   //query on page load
     async function curatedPhotos() {
@@ -55,14 +68,9 @@ async function fetchApi(url) {
 
   curatedPhotos();
 
-
+ 
 //Event Listeners
-panels.forEach((panel)=>{
-    panel.addEventListener('click', ()=>{
-        removeActiveClasses();
-        panel.classList.add('active');
-    });
-});
+
 
 btn.addEventListener('click', () => {
     search.classList.toggle('active')
